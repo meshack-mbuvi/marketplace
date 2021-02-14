c48b890a664da99ebd3e17ab73fa856f6cb3d06c
Meshack Mbuvi <meshmbuvi@gmail.com>
2021-02-14 23:38:45 +0300

    sell products.

--- src/contracts/Marketplace.sol
+++ src/contracts/Marketplace.sol
@@ -2,8 +2,48 @@ pragma solidity >=0.5.0;
 
 contract Marketplace {
   string public name;
+  uint public productCount = 0;
+  mapping(uint => Product) public products;
+
+  struct Product {
+    uint id;
+    string name;
+    uint price;
+    address owner;
+    bool purchased;
+  }
+
   constructor () public {
     name = "Dapp University Marketplace";
+  }
+
+  event ProductCreated(
+    uint id,
+    string name,
+    uint price,
+    address owner,
+    bool purchased
+    );
+
+  function createProduct(
+    string memory _name, uint _price) public {
+    require(bytes(_name).length > 0);
+    require(_price > 0 );
+
+    // increment product count
+    productCount++;
+    // create the product
+    products[productCount] = Product(productCount, _name, _price, msg.sender, false);
+    
+    // Triger an event
+    emit ProductCreated(
+      productCount,
+      _name,
+      _price,
+      msg.sender,
+      false
+      );
+
   }
 }
 
--- test/Marketplace.test.js
+++ test/Marketplace.test.js
@@ -1,24 +1,53 @@
+const { assert } = require('chai');
+require('chai').use(require('chai-as-promised')).should()
+
+/* eslint-disable no-undef */
 const Marketplace = artifacts.require('./Marketplace.sol');
 
-contract('Markeplace', (accounts)=>{
-  let markeplace;
+contract('Marketplace', ([deployer, seller, buyer]) => {
+  let marketplace;
 
-  before(async()=>{
-    markeplace = await Marketplace.deployed();
+  before(async () => {
+    marketplace = await Marketplace.deployed();
   })
 
-  describe('deployment', async()=>{
-    it('deploys successfully', async()=>{
-      const address = await markeplace.address
+  describe('deployment', async () => {
+    it('deploys successfully', async () => {
+      const address = await marketplace.address
       assert.notEqual(address, 0x0)
       assert.notEqual(address, '')
       assert.notEqual(address, null)
       assert.notEqual(address, undefined)
     })
 
-    it('has a name', async()=>{
-      const name = await markeplace.name()
+    it('has a name', async () => {
+      const name = await marketplace.name()
       assert.equal(name, 'Dapp University Marketplace')
     })
   })
+
+  describe('products', async () => {
+    let result, productCount;
+    before(async () => {
+      result = await marketplace.createProduct('Iphone X', web3.utils.toWei('1', 'Ether'), { from: seller });
+      productCount = await marketplace.productCount();
+    })
+
+    it('creates products', async () => {
+      // SUCCESS
+      assert.equal(productCount, 1)
+      const event = result.logs[0].args
+      assert.equal(event.name, 'Iphone X')
+      assert.equal(event.id.toNumber(), productCount.toNumber(), 'id is correct')
+      assert.equal(event.purchased, false, 'Purchased is correct')
+      assert.equal(event.owner, seller, ' seller is correct')
+
+      // FAILURE: must have a name
+      await await marketplace.createProduct('', web3.utils.toWei('1', 'Ether'), { from: seller }).should.be.rejected;
+
+      // FAILURE: must have a price
+      await await marketplace.createProduct('Iphone X', 0, { from: seller }).should.be.rejected;
+    })
+  })
+
 })
